16fd9fa1238e896c88675c69554d846dd2436000
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2022-01-31 11:47:12 +0700

    data pelanggaran

--- application/models/m_pelanggaran.php
+++ application/models/m_pelanggaran.php
@@ -2,6 +2,52 @@
 
 class m_progress extends CI_Model
 {
+    public function tampil_data()
+    {
+        $this->db->select(
+            '
+            SELECT a.no_pelanggaran, 
+            b.VENDOR_NAMA, 
+            a.tgl_kejadian, 
+            c.AREA_NAMA, 
+            a.no_spj, 
+            a.no_KHS, 
+            e.PAKET_DESKRIPSI, 
+            f.nama_kel_pelanggaran, 
+            a.evidence1, 
+            a.evidence2 
+        '
+        );
+
+        $this->db->from('tb_spj a');
+        $this->db->from('tb_vendor b');
+        $this->db->from('tb_area c');
+        $this->db->from('tb_spj d');
+        $this->db->from('tb_paket e');
+        $this->db->from('tb_master_kelompok_pelanggaran f');
+        $this->db->where('a.id_vendor = b.VENDOR_ID');
+        $this->db->where('a.area_kode = c.AREA_KODE');
+        $this->db->where('a.no_spj = d.SPJ_NO');
+        $this->db->where('d.PAKET_JENIS = e.PAKET_JENIS');
+        $this->db->where('a.id_kel_pelanggaran = f.id_kel_pelanggaran');
+        $this->db->where('a.status = 1');
+        $this->db->order_by('no_pelanggaran ');
+
+        $query = $this->db->get();
+        $result = $query->result();
+        return $result;
+    }
+
+    public function get_keyword($keyword)
+    {
+
+        $progress = $this->db->get('tb_pelanggaran_khs');
+        $this->db->select('*');
+        $this->db->from('tb_pelanggaran_khs');
+        $this->db->like('no_pelanggaran', $keyword);
+        return $this->db->get->result();
+    }
+
     public function inp_pel_khs()
     {
     }
@@ -15,6 +61,10 @@ class m_progress extends CI_Model
     }
 
     public function upl_sanksi_khs()
+    {
+    }
+
+    public function app_pel()
     {
     }
 }
