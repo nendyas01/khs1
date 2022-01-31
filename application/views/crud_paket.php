d160d0ab9907ac2b8ae6623ff58d941055985de4
nendyas01 <nndyshaharani@apps.ipb.ac.id>
2022-01-31 13:02:18 +0700

    Menambahkan data master edit paket

--- application/views/crud_paket.php
+++ application/views/crud_paket.php
@@ -1,15 +1,14 @@
 <div class="content-wrapper">
     <section class="content-header">
         <h1>
-            Pengelolaan Paket
+            DATA MASTER PAKET
             <small>Control panel</small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Input Data Paket</li>
+            <li class="active">Data Master Paket</li>
         </ol>
     </section>
-
     <section class="content">
         <div class="row">
             <div class="col-md-12">
@@ -27,7 +26,7 @@
                                         <th>Satuan</th>
                                         <th>Paket Deskripsi 2</th>
                                         <th>Status</th>
-                                        <th colspan="2">Aksi</th>
+                                        <th colspan="3">Aksi</th>
                                     </tr>
                                 </thead>
 
@@ -47,3 +46,61 @@
                                             <td><?php echo anchor('crud_paket/edit_crud_paket/' . $cp->PAKET_JENIS, '<div class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></div>') ?></td>
                                         </tr>
                                     <?php } ?>
+
+                                    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css">
+                                    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+                                    <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
+
+                                    <script>
+                                        $(document).ready(function() {
+                                            $('#example').DataTable();
+                                        });
+                                    </script>
+
+                                </tbody>
+                            </table>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </section>
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel"> Tambah Data Paket</h4>
+                </div>
+                <div class="modal-body">
+                    <form method="post" action="<?php echo base_url() . 'crud_skkio/tambah_aksi'; ?>">
+                        <div class="form-group">
+                            <label>Paket Jenis</label>
+                            <input type="text" name="id_anggaran" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Paket Deskripsi</label>
+                            <input type="text" name="no_surat" class="form-control">
+
+                            <div class="form-group">
+                                <label>Satuan </label>
+                                <input type="text" name="nama_pekerjaan" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Paket Deskripsi 2</label>
+                                <input type="text" name="pemberi_kerja" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Status</label>
+                                <input type="text" name="pic" class="form-control">
+                            </div>
+
+                            <button type="reset" class="btn btn-danger" data-dismiss="modal">Reset</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
