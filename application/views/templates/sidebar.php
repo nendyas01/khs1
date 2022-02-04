1e87db9ce40d6fc85e318f7899a17cfaff21d3fc
nendyas01 <nndyshaharani@apps.ipb.ac.id>
2022-02-04 10:14:23 +0700

    Menambahkan perijinan

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -63,7 +63,6 @@
         </a>
         <ul class="treeview-menu">
           <li><a href="<?php echo base_url('list_sanksi/survey') ?>"><i class="fa fa-circle-o"></i> BA Survey</a></li>
-          <li><a href="<?php echo base_url('list_sanksi') ?>"><i class="fa fa-circle-o"></i> List Sanksi</a></li>
           <li><a href="<?php echo base_url('list_sanksi/monitor_perijinan') ?>"><i class="fa fa-circle-o"></i> Monitoring Perijinan</a></li>
           <li><a href="<?php echo base_url('list_sanksi/pengajuan_perijinan') ?>"><i class="fa fa-circle-o"></i> Pengajuan Perijinan Baru</a></li>
           <li><a href="<?php echo base_url('list_sanksi/retribusi') ?>"><i class="fa fa-circle-o"></i> Retribusi</a></li>
