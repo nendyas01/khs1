0f5ca1fee7c9f25302c475120ed22b9ae715c1e6
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2022-02-10 11:02:14 +0700

    berdoa

--- application/controllers/mapping_vendor.php
+++ application/controllers/mapping_vendor.php
@@ -1,24 +1,25 @@
 <?php
-<<<<<<< HEAD
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class mappping_vendor extends CI_Controller
-{ 
-    public function __construct(){ 
-        parent::__construct(); 
-        $this->load->model('m_mapping_vendor'); 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_mapping_vendor');
     }
 
-    public function index(){ 
-        $data['anggaran'] = $this->m_anggaran->tampil_data(); 
-       $this->load->view('templates/header');
+    public function index()
+    {
+        $data['anggaran'] = $this->m_anggaran->tampil_data();
+        $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
-        $this->load->view('v_add_mapping_vendor',$data);
-        $this->load->view('templates/footer'); 
+        $this->load->view('v_add_mapping_vendor', $data);
+        $this->load->view('templates/footer');
         //print_r($data);
     }
 }
-=======
+
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class mapping_vendor extends CI_Controller
@@ -39,4 +40,3 @@ class mapping_vendor extends CI_Controller
         //print_r($data);
     }
 }
->>>>>>> c80bb28e3a949ab76cfe1ad020122fc657e07dd3
