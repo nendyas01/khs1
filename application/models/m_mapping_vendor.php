2c5c51620309c0c148d6abb1fc85734c8587bdb6
fathimahazzahratul <fathimahazzahratul@apps.ipb.ac.id>
2022-02-10 12:35:58 +0700

    merubah

--- application/models/m_mapping_vendor.php
+++ application/models/m_mapping_vendor.php
@@ -4,16 +4,10 @@ class m_mapping_vendor extends CI_Model{
     public function tambah_aksi()
 
     {
-        $this->db->select('PAKET_JENIS, 
-        VENDOR_ID, AREA_KODE, MAPPING_TAHUN, ZONE');
-        $this->db->from('tb_mapping_vendor');
-        $query = $this->db->get();
-        $result = $query->result();
-    
-    
+        
 } 
 
-public function getdata()
+public function getarea()
     {
         $query = $this->db->query("SELECT * FROM tb_area ORDER BY AREA_NAMA ASC");
         return $query->result();
