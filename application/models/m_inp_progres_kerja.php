b7dbc3ba8243543d7b625e740e127aea2abc4e59
khoiriyahnurul <khoiriyahnurul@apps.ipb.ac.id>
2022-02-22 08:56:47 +0700

    mencoba

--- application/models/m_inp_progres_kerja.php
+++ application/models/m_inp_progres_kerja.php
@@ -4,7 +4,9 @@ class m_inp_progres_kerja extends CI_Model
 {
     public function select_spj_no()
     {
-        $query = $this->db->query("SELECT DISTINCT a.spj_no FROM tb_spj a, tb_skko_i b WHERE a.skki_no = b.skki_no and b.area_kode = a.area_kode ORDER BY a.spj_no ASC");
+        $query = $this->db->query("SELECT DISTINCT a.spj_no 
+        FROM tb_spj a, tb_skko_i b 
+        WHERE a.skki_no = b.skki_no and b.area_kode = a.area_kode ORDER BY a.spj_no ASC");
         return $query->result();
     }
 }
