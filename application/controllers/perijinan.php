b04a43f343aa1a04cd3f1b93ffadfa9bf10d5258
nendyas01 <nndyshaharani@apps.ipb.ac.id>
2022-03-01 09:28:36 +0700

    Perijinan

--- application/controllers/perijinan.php
+++ application/controllers/perijinan.php
@@ -18,4 +18,29 @@ class perijinan extends CI_Controller
         $this->load->view('perijinan', $data);
         $this->load->view('templates/footer');
     }
+
+    public function perijinan_add()
+    {
+        /* $data['perijinan_add'] = $this->m_perijinan->perijinan(); */
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('perijinan_add' /* $data */);
+        $this->load->view('templates/footer');
+    }
+
+    public function tambah_aksi()
+    {
+        $AREA_KODE = $this->input->post('AREA_KODE');
+        $AREA_NAMA = $this->input->post('AREA_NAMA');
+        $AREA_ZONE = $this->input->post('AREA_ZONE');
+
+        $data = array(
+            'AREA_KODE'               => $AREA_KODE,
+            'AREA_NAMA'               => $AREA_NAMA,
+            'AREA_ZONE'               => $AREA_ZONE,
+        );
+
+        $this->m_perijinan->input_data($data, 'tb_area');
+        redirect('perijinan_add/index');
+    }
 }
--- application/models/m_perijinan.php
+++ application/models/m_perijinan.php
@@ -6,4 +6,9 @@ class m_perijinan extends CI_Model
     public function perijinan()
     {
     }
+
+    public function perijinan_add($where, $table)
+    {
+        return $this->db->get_where($table, $where);
+    }
 }
